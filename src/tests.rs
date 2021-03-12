9a1200206efa91bc77d88f75291e130c579015ea
Jon Fast <jnfstdj656@gmail.com>
2021-03-12 16:36:54 -0500

    fix tests

--- src/tests.rs
+++ src/tests.rs
@@ -6,9 +6,11 @@ mod tests {
     fn test_append_path() {
         let path1 = "C:\\Users";
         let path2 = "jfast\\Desktop";
+        let full_path = "C:\\Users\\jfast\\Desktop";
         let pp1 = PathParser::new(&String::from(path1));
         let pp2 = pp1.append_segment(&String::from(path2));
         let result = pp2.get_segment().get_default_segment_string();
-        assert_eq!(result, "C:\\Users\\jfast\\Desktop");
+        let comparator = PathParser::new(&String::from(full_path)).get_segment().get_default_segment_string();
+        assert_eq!(result, comparator);
     }
 }
